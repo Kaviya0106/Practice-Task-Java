bad2be129a57c8c99e66eb2c6e58984c58602307
KAVIYA V <129725854+Kaviya0106@users.noreply.github.com>
2025-12-09 20:56:09 +0530

    Add files via upload

---
+++ CheckNumberIsPositive.java
@@ -0,0 +1,18 @@
+package week1.task2;
+
+public class CheckNumberIsPositive {
+	int version=10;
+	public static void main(String[]args) {
+		int number=-5;
+		// Scanner sc=new Scanner(System.in);
+		//System.out.prinln("Enter A Number");
+		//int number=sc.nextInt();
+		if(number>0) {
+			System.out.println("Number is Positive");
+		}else {
+			System.out.println("Number is Negative");
+		}
+		
+	}
+
+}
---
+++ Chrome.java
@@ -0,0 +1,28 @@
+package week1.task1;
+
+public class Chrome {
+		public static void main(String[] args) {
+			float browserVersion=142.8f;
+			String browserName="Chrome";
+			boolean isVisible=true;
+			boolean isIncognito=false;
+			int releaseYear=2000;
+			char shortcutKey='c';
+			long totalHistoryCount=987654321;
+			short totalBookmarks=100;
+			
+		System.out.println("Browser Version:" + browserVersion);
+		System.out.println("Browser Name:" + browserName);
+	    System.out.println("Is Browser Visible: " + isVisible);
+	    System.out.println("Release Year: " + releaseYear);
+	    System.out.println("Incognito Mode: " + isIncognito);
+	    System.out.println("Shortcut Key Pressed: " + shortcutKey);
+	    System.out.println("Total History Count: " + totalHistoryCount);
+	    System.out.println("Total Bookmarks:" + totalBookmarks);
+	}
+	}
+
+
+
+
+}
