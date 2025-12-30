136448c5a1c13ffb673214dc9774068d4c027fbc
KAVIYA V <129725854+Kaviya0106@users.noreply.github.com>
2025-12-31 01:55:03 +0530

    Update SwapTwoNumbers.java

--- SwapTwoNumbers.java
+++ SwapTwoNumbers.java
@@ -1,5 +1,30 @@
+/Swapping Two Numbers using Third variable 
+ //Before Swapping 
+  a=10 
+  B=20
+//After Swapping 
+  a=20
+  b=10
+
+
 package week1.day2;
 
 public class SwapTwoNumbers {
+  public static void main(String[] args) { 
 
-}
+	        int a = 10;      // First number
+	        int b = 20;      // Second number
+	        int temp;        // Temporary variable for swapping
+
+	        temp = a;        //  Store value of 'a' in temp
+	        a = b;           //  Assign value of 'b' to 'a'
+	        b = temp;        //  Assign stored value from temp to 'b'
+
+	        // Display swapped values
+	        System.out.println("a = " + a);
+	        System.out.println("b = " + b);
+	    }
+	}
+
+
+
